a2dec399ab395c21a265ab77f0ca469d047060e8
Scorpio6905 <saadahmed13475@gmail.com>
2026-09-02 14:12:31 +0500

    RegFile

---
+++ src/main/scala/regfile/CodeMem.scala
@@ -0,0 +1,30 @@
+package regfile
+
+
+import chisel._
+import chisel3.util._
+import chisel3.util.experimental.loadMemoryFromFile
+import scala.io.Source
+
+object Generate_ProcessorTile extends App {
+    var initFile = "src/test/resources/main.txt"
+
+    chisel3.Driver.execute(args , () => new ProcessorTile ( initFile ))
+}
+
+class InstMemIO extends Bundle with Config{
+    val addr = Input (UInt(WLEN.W))
+    val inst = Output (UInt(WLEN.W))
+
+}
+
+class InstMem ( initFile : String ) extends Module with Config {
+    val io = IO (new InstMemIO)
+
+    // INST_MEM_LEN in Bytes or INST_MEM_LEN / 4 in words
+    val imem = Mem (INST_MEM_LEN , UInt(WLEN.W))
+
+    loadMemoryFromFile(imem , initFile)
+
+    io.inst := imem(io.addr / 4.U)  
+}
